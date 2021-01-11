7ce054ac0475d659f1933d83ee1e28d67e341c62
ehtesham1999 <ehteshamhussain1999@gmail.com>
2021-01-11 15:50:47 +0530

    api end points working

--- app.py
+++ app.py
@@ -25,29 +25,47 @@ def load_recommendations():
     item_similarity_df = pd.read_csv("static/prod_similarity.csv", index_col=0)
     print("item_similarity_df cached in memory")
     return item_similarity_df
+def load_products():
+    product_ratings = pd.read_csv("static/prod_ratings.csv")
+    return product_ratings
+
 
 
 item_similarity_df = load_recommendations()
+product_ratings = load_products()
 # item_similarity_df = cache.ram('item_similarity_df3', load_recommendations, None)
 # print(item_similarity_df.head())
+def prod_name(prodid):
+    return product_ratings.loc[product_ratings['prod_ID'] == prodid ].iloc[0]['prod_name']
 
-
-def get_similar_movies(movie_name, user_rating):
+def get_similar_products(prod_name, user_rating):
     try:
-        similar_score = item_similarity_df[movie_name] * (user_rating - 2.5)
-        similar_movies = similar_score.sort_values(ascending=False)
+        similar_score = item_similarity_df[prod_name] * (user_rating - 2.5)
+        similar_prods = similar_score.sort_values(ascending=False)
     except:
-        print("don't have movie in model")
-        similar_movies = pd.Series([])
+        print("don't have product in model")
+        similar_prods = pd.Series([])
 
-    return similar_movies
+    return similar_prods
 
 
-def check_seen(recommended_movie, watched_movies):
-    for movie_id, movie in watched_movies.items():
-        movie_title = movie["title"]
+# def recommendations(items):
+#     similar_products = pd.DataFrame()
+#     for product,rating in items:
+#         similar_products = similar_products.append(get_similar_products(product,rating),ignore_index = True)
+#
+#     final_results = pd.DataFrame(similar_products.sum().sort_values(ascending=False).head(20))
+#     final_results.reset_index(inplace = True)
+#     final_results.columns = ['prodid','rating']
+#     final_results['prodname'] = final_results['prodid'].apply(prod_name)
+#     return final_results
 
-        if recommended_movie == movie_title:
+
+def check_seen(recommended_product, all_products):
+    for prod_id, product in all_products.items():
+        prod_title = product["prodId"]
+
+        if recommended_product == prod_title:
             return True
 
     return False
@@ -57,25 +75,42 @@ def check_seen(recommended_movie, watched_movies):
 @app.route('/getrecommendations', methods=['POST'])
 def get_recommendations():
     content = request.get_json()
-    watched_movies = content["watched_movies"]
-    similar_movies = pd.DataFrame()
+    all_products = content["all_products"]
+    similar_products = pd.DataFrame()
 
-    for movie_id, movie in watched_movies.items():
-        similar_movies = similar_movies.append(get_similar_movies(movie["title"], movie["rating"]), ignore_index=True)
+    for proid_id, product in all_products.items():
+        print(product["prodId"], product["rating"])
+        similar_items = similar_products.append(get_similar_products(product["prodId"], product["rating"]), ignore_index=True)
 
-    all_recommend = similar_movies.sum().sort_values(ascending=False)
 
-    recommended_movies = []
-    for movie, score in all_recommend.iteritems():
-        if not check_seen(movie, watched_movies):
-            recommended_movies.append(movie)
+    #all_recommend = similar_items.sum().sort_values(ascending=False)
 
-    if len(recommended_movies) > 100:
-        recommended_movies = recommended_movies[0:100]
+    final_results = pd.DataFrame(similar_items.sum().sort_values(ascending=False).head(20))
+    final_results.reset_index(inplace=True)
+    final_results.columns = ['prodid', 'rating']
+    final_results['prodname'] = final_results['prodid'].apply(prod_name)
 
-    print(recommended_movies)
-    return dict({"recommm":recommended_movies})
-    #return None
+    return final_results.to_json()
+
+
+
+
+    # recommended_prods = []
+    # for prod, score in all_recommend.iteritems():
+    #     if not check_seen(prod, all_products):
+    #         recommended_prods.append(prod)
+    #
+    # recon_prods = {}
+    # for i in range(len(recommended_prods)):
+    #     recon_prods['']prod_name(recommended_prods[i])
+    #
+    # if len(recommended_prods) > 100:
+    #     recommended_prods = recommended_prods[0:100]
+    #
+    # print(recommended_prods)
+    # #return dict({"recommm":recommended_movies})
+    # json = dataFrame.to_json()
+    # return dict({"hello":"world"})
 
 
 
